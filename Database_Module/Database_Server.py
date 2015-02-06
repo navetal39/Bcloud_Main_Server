d15976e4a060732e0963907ec188d4fe24976c99
Tamir SH <tamir2341@gmail.com>
2015-02-06 11:55:10 +0200

    Fixed conventions in the other one...
    Sorry for not doing this in the previous commit... So just if you didn't
    understand the previous one, so:
    Read the conventions again, you-. :page_facing_up: (:P)
    
    * I also fixed one syntax error, in line 38/39.

--- Database_Module/Database_Server.py
+++ Database_Module/Database_Server.py
@@ -5,37 +5,38 @@
 from Database_Management import *
 import socket
 import select
-open_sockets=[]
 
-def Respond_To_Clients(to_do_list, write_list):
-    new_to_do_list=[]
+open_sockets = []
+
+def respond_to_clients(to_do_list, write_list):
+    new_to_do_list = []
     for pair in to_do_list:
-        target, data=pair
+        target, data = pair
         if target in write_list:
-            info=data.split(';')
-            flag=parts[0]
+            info = data.split(';')
+            flag = parts[0]
             info.remove(flag)
             
             if flag=="REG":
-                status=Register_New_User(info[0], info[1])
+                status = register_new_user(info[0], info[1])
                 
             elif flag=="AUT":
-                status=Authenticate(info[0], info[1])
+                status = authenticate(info[0], info[1])
                 
             elif flag=="EXI":
-                status=Name_Exists(info[0])
+                status = name_exists(info[0])
                     
             open_socket.send(status+';'+data)
-            print "Sent data to client"
+            print "Sent data to client" # -For The Record-
         else:
             updated_ml.append(pair)
     return new_to_do_list
 
 #def Main():
-    to_do_list=[]
-    server_socket=socket.socket()
+    to_do_list = []
+    server_socket = socket.socket()
     server_socket.bind(('0.0.0.0', 6853))
-    server_socket.lister(6)
+    server_socket.listen(6)
 
     while True:
         read_list, write_list, exception_list = select.select([server_socket]+open_sockets, open_sockets, [])
@@ -43,12 +44,12 @@ def Respond_To_Clients(to_do_list, write_list):
             if open_socket in server_socket:
                 new_client_soclet, client_addr = open_socket.accept()
                 open_sockets.append(new_client_socket)
-                print "Client accepted"
+                print "Client accepted"  # -For The Record-
             else:
-                data=open_socket.recv(1024)
+                data = open_socket.recv(1024)
                 if rdata == '':
                     open_sockets.remove(open_socket)
-                    print "Client disconnected"
+                    print "Client disconnected"  # -For The Record-
                 else:
                     to_do_list.append((open_socket, data))
 
