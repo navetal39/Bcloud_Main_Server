d5f905e8ea6313704933d88b234bd0b409bf85fc
Tamir SH <tamir2341@gmail.com>
2015-02-06 11:47:30 +0200

    Fixed conventions
    read the conventions again, you-. :page_facing_up: (:P)

--- Database_Module/Database_Management.py
+++ Database_Module/Database_Management.py
@@ -4,42 +4,42 @@
 # ===================================
 
 import socket
-MEMORY_IP='0.0.0.0'
-MEMORY_PORT=9077
-MEMORY_SOCKET=socket.socket()
+MEMORY_IP = '0.0.0.0'
+MEMORY_PORT = 9077
+MEMORY_SOCKET = socket.socket()
 
-def Start_Run():
+def start_run():
     '''This method will run every time you boot up the module.
     '''
     MEMORY_SOCKET.connect((MEMORY_IP, MEMORY_PORT))
-    database_file=open('database.txt', 'r')
+    database_file = open('database.txt', 'r')
     encrypted_database_content = database_file.read()
-    database_content=Decrypt(encrypted_database_content)
-    database_lines=database_content.split('\n')
+    database_content = decrypt(encrypted_database_content)
+    database_lines = database_content.split('\n')
     global dict_database
-    dict_database={}
+    dict_database = dict()
     for line in database_lines:
-        if line!="":
-            name, password=line.split(':')
-            dict_database[name]=password
+        if line != "":
+            name, password = line.split(':')
+            dict_database[name] = password
     database_file.close()
 
-def Decrypt(data):
+def decrypt(data):
     '''Decrypts the data it gets. Unimplimented.
     '''
     return data
 
-def Make_Folder(name):
+def make_folder(name):
     '''This method is dedicated to communicating with the Storage module for setting up new directories.
     '''
     try:
-        MEMORY_SOCKET.send("Make;"+name)
-        response=MEMORY_SOCKET.recv(1024)
+        MEMORY_SOCKET.send("Make;" + name)
+        response = MEMORY_SOCKET.recv(1024) # deal with recieving a full response... thing with leangth, if needed.
         return response
     except:
         return "Unknown error"
 
-def Name_Exists(name):
+def name_exists(name):
     '''Verifies that a given name exists in the database
     '''
     try:
@@ -50,27 +50,27 @@ def Name_Exists(name):
     except:
         return "Unknown error"
 
-def Register_New_User(username, password):
+def register_new_user(username, password):
     '''A method for registering new users.
     '''
     try:
-        if Name_Exists(username):
+        if name_exists(username):
             return 'Name in use'
         else:
-            dict_database[username]=password
-            database=open('database.txt', 'a')
+            dict_database[username] = password
+            database = open('database.txt', 'a')
             print >>database, '{n}:{p}'.format(n=username, p=password)
             database.close()
-            return Make_Folder(name)
+            return make_folder(name)
     except:
         return "Unknown error"
 
-def Authenticate(name, password):
+def authenticate(name, password):
     '''Verifies that a given name and a given password matches in the database
     '''
     try:
-        if Name_Exists(name):
-            if dict_database[name]==password:
+        if name_exists(name):
+            if dict_database[name] == password:
                 return "Success"
             else:
                 return "Authentication Failed"
@@ -83,7 +83,7 @@ def Authenticate(name, password):
 try:
     dict_database[0]
 except:
-    Start_Run()
+    start_run()
 
 '''
 Exciting. Satisfying. Period.
