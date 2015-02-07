03f8140056dabe2cbcf721ca27cb9393bbaa4095
Tamir SH <tamir2341@gmail.com>
2015-02-07 18:52:05 +0200

    Minor changes and syntax fixing
    I looked at the code and I found some syntax errors so I fixed them...
    :banana: :P

--- Database_Module/Database_Management.py
+++ Database_Module/Database_Management.py
@@ -9,13 +9,15 @@ MEMORY_PORT = 9077
 MEMORY_SOCKET = socket.socket()
 
 def start_run():
-    '''This method will run every time you boot up the module.
+    ''' This method will run every time you boot up the module.
     '''
     MEMORY_SOCKET.connect((MEMORY_IP, MEMORY_PORT))
+    
     database_file = open('database.txt', 'r')
     encrypted_database_content = database_file.read()
     database_content = decrypt(encrypted_database_content)
     database_lines = database_content.split('\n')
+    
     global dict_database
     dict_database = dict()
     for line in database_lines:
@@ -24,13 +26,14 @@ def start_run():
             dict_database[name] = password
     database_file.close()
 
+
 def decrypt(data):
-    '''Decrypts the data it gets. Unimplimented.
+    ''' Decrypts the data it gets. Unimplimented.
     '''
     return data
 
 def make_folder(name):
-    '''This method is dedicated to communicating with the Storage module for setting up new directories.
+    ''' This method is dedicated to communicating with the Storage module for setting up new directories.
     '''
     try:
         MEMORY_SOCKET.send("Make;" + name)
@@ -40,7 +43,7 @@ def make_folder(name):
         return "Unknown error"
 
 def name_exists(name):
-    '''Verifies that a given name exists in the database
+    ''' Verifies that a given name exists in the database
     '''
     try:
         if name in dict_database.keys():
@@ -51,22 +54,22 @@ def name_exists(name):
         return "Unknown error"
 
 def register_new_user(username, password):
-    '''A method for registering new users.
+    ''' A method for registering new users.
     '''
     try:
         if name_exists(username):
             return 'Name in use'
         else:
             dict_database[username] = password
-            database = open('database.txt', 'a')
-            print >>database, '{n}:{p}'.format(n=username, p=password)
+            database = open('database.txt', 'a') #
+            print >>database, '{n}:{p}'.format(n=username, p=password) #
             database.close()
             return make_folder(name)
     except:
         return "Unknown error"
 
 def authenticate(name, password):
-    '''Verifies that a given name and a given password matches in the database
+    ''' Verifies that a given name and a given password matches in the database
     '''
     try:
         if name_exists(name):
--- Database_Module/Database_Server.py
+++ Database_Module/Database_Server.py
@@ -14,25 +14,25 @@ def respond_to_clients(to_do_list, write_list):
         target, data = pair
         if target in write_list:
             info = data.split(';')
-            flag = parts[0]
+            flag = parts[0] #
             info.remove(flag)
             
-            if flag=="REG":
+            if flag == "REG":
                 status = register_new_user(info[0], info[1])
                 
-            elif flag=="AUT":
+            elif flag == "AUT":
                 status = authenticate(info[0], info[1])
                 
-            elif flag=="EXI":
+            elif flag == "EXI":
                 status = name_exists(info[0])
                     
             open_socket.send(status+';'+data)
             print "Sent data to client" # -For The Record-
         else:
-            updated_ml.append(pair)
+            new_to_do_list.append(pair)
     return new_to_do_list
 
-#def Main():
+#def Main():    ##
     to_do_list = []
     server_socket = socket.socket()
     server_socket.bind(('0.0.0.0', 6853))
@@ -41,13 +41,13 @@ def respond_to_clients(to_do_list, write_list):
     while True:
         read_list, write_list, exception_list = select.select([server_socket]+open_sockets, open_sockets, [])
         for open_socket in read_list:
-            if open_socket in server_socket:
-                new_client_soclet, client_addr = open_socket.accept()
+            if open_socket is server_socket:
+                new_client_socket, client_addr = open_socket.accept()
                 open_sockets.append(new_client_socket)
                 print "Client accepted"  # -For The Record-
             else:
                 data = open_socket.recv(1024)
-                if rdata == '':
+                if data == '':
                     open_sockets.remove(open_socket)
                     print "Client disconnected"  # -For The Record-
                 else:
