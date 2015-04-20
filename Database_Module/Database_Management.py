06c6ef5111465e8b23ab931c6bad19950cefbbbc
Nave <navetal@gmail.com>
2015-04-20 14:08:21 +0300

    Documentation

--- Database_Module/Database_Management.py
+++ Database_Module/Database_Management.py
@@ -16,33 +16,45 @@ class DataBase(object):
         self.MEMORY_SOCKET = socket.socket()
         self.dict_database = {}
         
+        # Initial database setup - Reading the existing entries from the .txt file and converting it to a dictionary form.
         database_file = open('database.txt', 'r')
         encrypted_database_content = database_file.read()
-        encrypted_lines = encrypted_database_content.split('���')
+        encrypted_lines = encrypted_database_content.split('§§§') #The database isn't completly encrypted, but rather each of
+                                                                  # it's lines (entries) are, and each entry is seperated
+                                                                  # from it's neighbours with '§§§'
         for encrypted_line in encrypted_lines:
             line = crypto.decrypt(encrypted_line)
-            if line != "":
+            if line != "": # Sometimes there are empty lines in the .txt file.
+                           # For example - since each entry is written into the DB as "blablabla§§§", the function will always thing there's one empty entry at the end.
                 name, password = line.split(':')
                 self.dict_database[name] = password
         database_file.close()
 
     def __str__(self):
+        ''' Used for debugging
+        '''
         info = ''
         for key in self.dict_database.keys():
             info += "{name}: {pw}\n".format(name=key, pw=self.dict_database[key])
         return info
     
     def __repr__(self):
+    ''' Used for debugging
+    '''
         info = ''
         for key in self.dict_database.keys():
             info += "{name}:{pw}\n".format(name=key, pw=self.dict_database[key])
         return info
 
     def connect(self):
+        ''' Connects to the memory module. Used for convinience.
+        '''
         self.MEMORY_SOCKET = socket.socket()
         self.MEMORY_SOCKET.connect((self.MEMORY_IP, self.MEMORY_PORT))
 
     def disconnect(self):
+        ''' Disconencts frm the memory module. Used for convinience.
+        '''
         self.MEMORY_SOCKET.close()
 
     def make_folder(self, name):
@@ -81,12 +93,12 @@ class DataBase(object):
         '''
         try:
             if self.name_exists(username) == "SCS":
-                return 'NIU'
+                return 'NIU' # If the "name exists" method returns "success", it means the name exists in the database, so the name is in use.
             elif self.name_exists(username) == "NNM":
                 self.dict_database[username] = password
                 database = open('database.txt', 'a')
                 encrypted_data = crypto.encrypt('{n}:{p}'.format(n=username, p=password))
-                print >>database, encrypted_data+'���'
+                print >>database, encrypted_data+'§§§' # Adds the entry to the database with our chosen seperation mark
                 database.close()
                 return self.make_folder(username)
             else:
