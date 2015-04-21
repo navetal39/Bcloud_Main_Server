460f573b642431e7a038d1fbc1cec5f55aa65d4b
Tamir SH <tamir2341@gmail.com>
2015-04-21 18:18:30 +0300

    crypto docs

--- crypto.py
+++ crypto.py
@@ -1,9 +1,14 @@
+# Imports: #
 from Crypto.Cipher import AES
 
+# Constants for ecryption: #
 BLOCK_SIZE = 32
 PADD_CHAR = '?'
 
+# Ecryption funcs: #
 def encrypt(plaintext):
+	''' Encrypts the plaintext received.
+	'''
     from crypto_extended import generate_key as KEY
     padded_plaintext = padd(plaintext)
     encryptor = AES.new(KEY())
@@ -12,20 +17,26 @@ def encrypt(plaintext):
     return encoded_ciphertext
 
 def decrypt(encoded_ciphertext):
+	''' Decrypts the Base64 encoded ciphertext received.
+	'''
     from crypto_extended import generate_key as KEY
     decryptor = AES.new(KEY())
     ciphertext = encoded_ciphertext.decode("Base64") #Decode from base64, the reason for encoding is written above.^
     padded_plaintext = decryptor.decrypt(ciphertext)
     return depadd(padded_plaintext)
 
-#Utill:
+## Utill for encryption funcs: ##
 def padd(not_padded):
+	''' Pad the plaintext before it is encrypted, so it will work with the AES block cipher.
+	'''
     padding_amount = (BLOCK_SIZE - len(not_padded) % BLOCK_SIZE)
     padding = padding_amount * PADD_CHAR
     padded = not_padded + padding
     return padded
 
 def depadd(padded):
+	''' De-pad the plaintext efter it was decrypted.
+	'''
     depadded = padded.strip(PADD_CHAR)
     return depadded
 
